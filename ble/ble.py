dadcad82103b7bf824d2dc115d06c720d06c78e9
Cheolhwan Lee <chlee4831@gmail.com>
2023-04-17 15:19:16 +0900

    Ble disconnect and reconnect

--- ble/ble.py
+++ ble/ble.py
@@ -24,28 +24,38 @@ async def scan():
     # print(devices)
     # print(devices)
     for dev in devices:
-        if dev.name.split("_")[0] == 'ADL-CHLEE':
+        if dev.name.split("_")[0] == 'ADL':
             target_devices.append(dev)
     return target_devices
 
+def search_address(address):
+    for device in device_list:
+        if device.dev.address == address:
+            return device_list.index(device)
+    return None
+
 def disconnected_callback(client):
     print(f'Device {client.address} disconnected, reason')
-    print(client)
+    
+    index = search_address(client.address)
+    if index is not None:
+        device = device_list.pop(index)
+        device.process.terminate()
+        del device
 
 async def ble_main():
-    task_list = []
-
     while True:
         try:
             target_devices = await scan()
             for dev in target_devices:
-                if str(dev) not in task_list:
-                    # print(dev, "find")
+                index = search_address(dev.address)
+                if index is None:
+                # if str(dev) not in task_list:
+                    print(dev, "find")
 
                     device = Device(dev)
                     await device.ble_client_start(disconnected_callback)
                     
-                    task_list.append(str(dev))
                     device_list.append(device)
 
         except:
